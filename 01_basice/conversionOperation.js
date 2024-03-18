d7da4d0e81b482aee3dd0079a143567c051ad091
sarvesh shahi <sarveshshahi1210@gmail.com>
2024-03-18 17:36:01 +0530

    conversion on data type

---
+++ 01_basice/conversionOperation.js
@@ -0,0 +1,25 @@
+// let score =33
+
+// console.log(typeof score);
+
+// //conversion string to number
+
+// let string="123abc"
+// let valueInNumber = Number(string);
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+// //"33"=33
+// //"33abc"=NaN
+
+
+// //any thing to boolean
+
+// let isLoggedIn =undefined
+// let booleanIsLogedIn=Boolean(isLoggedIn);
+// console.log(typeof(booleanIsLogedIn))
+// console.log(booleanIsLogedIn)
+
+let someNumber=33;
+let someNumberInString=String(someNumber)
+console.log(someNumberInString)
+console.log(typeof someNumberInString)
