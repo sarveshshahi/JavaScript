ac90b2c4456cfae924e28700a16d65920d6eb6b0
sarvesh shahi <sarveshshahi1210@gmail.com>
2024-03-18 15:35:22 +0530

    Privimit data type

---
+++ 01_basice/DataType.js
@@ -0,0 +1,27 @@
+"use strict"; //Defines the Javascript code should be execuited in "strict mode" its means=> the "use strict" directive was new in ECMAscript version 5.
+// it is not a statement, but a literal expression,ignored by earlier verions of java script.
+
+// alert(3+3); it run only broser not in node
+
+//Java script is a daynamic type language .it means any veriables can be assigned values of all types.
+
+let foo = 42; // foo is now a number
+foo = "bar"; // foo is now a string
+foo = true; //foo is now a boolean
+
+// JavaScript is also a weakly typed language, which means it allows implicit type conversion when an operation involves mismatched types, instead of throwing type errors.
+
+const a=22;
+const result=(a+"aman");
+
+
+//DataTypes
+/*
+int=2 to power of 53
+String
+Boolean
+bigint
+Null
+UNdefined
+Symbol
+ */
