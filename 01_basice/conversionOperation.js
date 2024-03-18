5847bacbfc896ade669a985f1f6d49da8e2cd292
sarvesh shahi <sarveshshahi1210@gmail.com>
2024-03-18 18:11:20 +0530

    opertaion

--- 01_basice/conversionOperation.js
+++ 01_basice/conversionOperation.js
@@ -23,3 +23,43 @@ let someNumber=33;
 let someNumberInString=String(someNumber)
 console.log(someNumberInString)
 console.log(typeof someNumberInString)
+
+///******************operation///////////////////// */
+
+// console.log(2+3);
+// console.log(3-4);
+// console.log(2*2);
+// console.log(2**3)
+// console.log(2/2);
+// console.log(2%2);
+
+
+// add two string///
+
+// let str1="aman";
+// let str2="shahi";
+// let str3=str1+str2;
+// console.log(str3)
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2")
+
+// let a=2;
+// let b=a++;
+// console.log(a , b)
+// let x=2
+// let y=++x;
+// console.log(x,y)
+// console.log(null ==0)
+// console.log(null>=0)
+// console.log(null<0)
+
+
+// console.log(undefined==0)
+// console.log(undefined>0)
+// console.log(undefined<0)
+
+//==
+console.log(2 ===2)
