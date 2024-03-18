c018b59076d1d4ae5002a04f4f93d4b460e0a123
sarvesh shahi <sarveshshahi1210@gmail.com>
2024-03-18 14:49:23 +0530

    Learn about veriables.

---
+++ 01_basice/variables.js
@@ -0,0 +1,16 @@
+ const name="aman";
+ let email="aman@gmail.com";
+ var password="12345"
+ ifCode="877653423egf"
+ let acountstate;
+
+//  name="rohan";
+ 
+email="sarvesh@gmail.com"
+password="2345";
+ifCode="8t76ygf"
+/*
+var is not used
+ */
+//  console.log(aman);
+ console.table([name,password,ifCode,email,acountstate])
