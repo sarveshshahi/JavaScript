70fe9b1e7096147c80bb2c6557734c11f7572196
sarvesh shahi <sarveshshahi1210@gmail.com>
2024-03-19 12:14:03 +0530

    string

---
+++ 01_basice/String.js
@@ -0,0 +1,33 @@
+//  let myname="sarvesh";
+//  let repocount=50
+
+
+// console.log(myname+repocount+"value");
+
+// console.log(`my name is ${myname} my repcount is ${repocount}`)
+
+// console.log(myname.length)
+
+// const gamename=new String('hitesh-hc')
+// console.log(gamename[0])
+
+// console.log(gamename.toUpperCase)
+// console.log(gamename.__proto__)
+// console.log(gamename.toUpperCase())
+// console.log(gamename.charAt(3))
+// console.log(gamename.indexOf('e'))
+
+// const newstring=gamename.substring(0,4)
+// console.log(newstring);
+
+//  const revsubstring=gamename.slice(-8,4)
+//  console.log(revsubstring);
+
+let Myname="      aman       ";
+console.log(Myname)
+console.log(Myname.trim());
+
+const url="https://sarveshshahi.com/sarvesh%20Shahi";
+console.log(url.replace('%20','-'))
+
+console.log(url.replace('20'))
