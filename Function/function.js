f15746d7008aa80e4185329384a60a12f15f3ad9
sarvesh shahi <sarveshshahi1210@gmail.com>
2024-05-07 23:33:53 +0530

    function.js

---
+++ Function/function.js
@@ -0,0 +1,86 @@
+function sayMyname(){
+    console.log("S"),
+    console.log("A"),
+    console.log("R"),
+    console.log("V"),
+    console.log("E"),
+    console.log("S"),
+    console.log("H")
+}
+
+
+//sayMyname()
+
+// function addTwoNumber(num1,num2){
+//     console.log(num1+num2)
+// }
+// const result=addTwoNumber(3,4);
+
+
+function addTwoNumber(num1,num2){
+    // let result=(num1+num2)
+    // return result;
+    // console.log(hello)
+    //after return nothing working in function
+
+    return(num1+num2)
+}
+
+
+// const result=addTwoNumber(3,4)
+
+// console.log("result:" ,result)
+
+
+
+// function UserLogin(UserName){
+//     if(UserName===undefined){
+//         console.log("plz enter username")
+//         return
+//     }
+//      return `${UserName} just login`
+// }
+
+// // console.log(UserLogin("aman"))
+// console.log(UserLogin())
+
+// function calculateCartPrice(...num1){
+//     return num1;
+// }
+
+// console.log(calculateCartPrice(200,300,400))
+
+
+// let user={
+//     userName:"sarvesh",
+//     price:300
+// }
+
+// function login(myobj){
+//     console.log(`my user name is ${myobj.userName} my couser price ${myobj.price}`)
+//     return
+// }
+
+// // console.log(login(user))
+
+// login({
+//     userName:"aman",
+//     price:200,
+// })
+
+let arr=[10,20,30,40,50]
+
+function returnArr(getArr){
+   return getArr[1]
+}
+//  console.log(returnArr(arr))
+
+console.log( returnArr([200,3400,6000,400]))
+
+
+
+
+
+
+
+ 
