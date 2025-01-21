f674210a670d3d8e70ad8f3b4f7d133fc27feb0d
SARVESH SHAHI <118737457+sarveshshahi@users.noreply.github.com>
2025-01-21 19:33:30 +0530

    add evcent

--- HightOrderLoop/map.js
+++ HightOrderLoop/map.js
@@ -17,10 +17,30 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////chaining//////////////////////////////////////////////////////////////////////////
 
-const Mynums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const Mynums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const numbers = Mynums.map((num) => num * 10)
-  .map((num) => num + 1)
-  .filter((num) => num >= 40);
+// const numbers = Mynums.map((num) => num * 10)
+//   .map((num) => num + 1)
+//   .filter((num) => num >= 40);
 
-console.log(numbers);
+// console.log(numbers);
+
+
+// const names = ["aman", "sarvesh", "harish", "ram", "gita", "sita"];
+
+// const newNames=names.map((name,index)=>(
+//   `${index}.${name}`
+// ))
+// console.log(newNames)
+
+// const numbers = [10, 20, 30];
+// const newnumbers=numbers.map((number,index)=>(
+//   `index:${index}, value:${number}`
+// ))
+// console.log(newnumbers)
+
+const names=["aman","sita","gita","Rohan","RAMEsh"]
+const search="r"
+
+const newname=names.filter((item)=>(item.toLowerCase().includes(search.toLowerCase())))
+console.log(newname)
